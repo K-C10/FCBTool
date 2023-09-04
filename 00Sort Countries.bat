e4623938b337a4818e0c375b9460ee5d0a522455
K-C10 <105539461+K-C10@users.noreply.github.com>
2023-09-03 18:26:05 -0600

    Batch file changes
    There was an error with the manual batch file, causing it not to compile the files into a jar file

---
+++ 00Sort Countries.bat
@@ -0,0 +1,62 @@
+title Sort
+
+
+mkdir "North America"
+move "North America-*.png" "%CD%\North America"
+
+mkdir "South America"
+move "South America-*.png" "%CD%\South America"
+
+mkdir "Europe"
+move "Europe-*.png" "%CD%\Europe"
+
+mkdir "Asia"
+move "Asia-*.png" "%CD%\Asia"
+
+mkdir "Africa"
+move "Africa-*.png" "%CD%\Africa"
+
+mkdir "Australia"
+move "Australia-*.png" "%CD%\Australia"
+
+
+cd Asia
+mkdir "Middle East"
+mkdir "Central Asia"
+mkdir "South Asia"
+mkdir "East and Southeast Asia"
+
+move "Asia-Armenia.png" "Middle East"
+move "Asia-Azerbaijan.png" "Middle East"
+move "Asia-Bahrain.png" "Middle East"
+move "Asia-Georgia.png" "Middle East"
+move "Asia-Iran.png" "Middle East"
+move "Asia-Iraq.png" "Middle East"
+move "Asia-Israel.png" "Middle East"
+move "Asia-Jordan.png" "Middle East"
+move "Asia-Kuwait.png" "Middle East"
+move "Asia-Lebanon.png" "Middle East"
+move "Asia-Oman.png" "Middle East"
+move "Asia-Qatar.png" "Middle East"
+move "Asia-Saudi Arabia.png" "Middle East"
+move "Asia-Syria.png" "Middle East"
+move "Asia-Turkey.png" "Middle East"
+move "Asia-United Arab Emirates.png" "Middle East"
+
+move "Asia-Kazakhstan.png" "Central Asia"
+move "Asia-Kyrgyzstan.png" "Central Asia"
+move "Asia-Russia.png" "Central Asia"
+move "Asia-Tajikistan.png" "Central Asia"
+move "Asia-Turkmenistan.png" "Central Asia"
+move "Asia-Uzbekistan.png" "Central Asia"
+
+move "Asia-Afghanistan.png" "South Asia"
+move "Asia-Bangladesh.png" "South Asia"
+move "Asia-Bhutan.png" "South Asia"
+move "Asia-India.png" "South Asia"
+move "Asia-Maldives.png" "South Asia"
+move "Asia-Nepal.png" "South Asia"
+move "Asia-Pakistan.png" "South Asia"
+move "Asia-Sri Lanka.png" "South Asia"
+
+move "Asia-*.png" "East and Southeast Asia"
